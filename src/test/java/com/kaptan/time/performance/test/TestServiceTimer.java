28ba4ea7187276b020595e8f65414ddc9c59b190
kaptan <mustafakapdan@gmail.com>
2015-10-25 23:27:36 +0300

    test modified

--- src/test/java/com/kaptan/time/performance/test/TestServiceTimer.java
+++ src/test/java/com/kaptan/time/performance/test/TestServiceTimer.java
@@ -1,43 +1,46 @@
 package com.kaptan.time.performance.test;
 
-import com.kaptan.service.logger.LogTimeLevel;
 import com.kaptan.service.logger.PerformanceTimeLogger;
+import com.kaptan.service.logger.TimerFormatterConstans;
 
 public class TestServiceTimer {
 
 	public static void main(String[] args) {
 
-		String logLevelStr = System.getProperty("log.level");
-		LogTimeLevel logLevel = LogTimeLevel.getByName(logLevelStr);
-		
-		PerformanceTimeLogger.setLogTimeLevel(logLevel);
-		PerformanceTimeLogger.getChronometer().start();
-
-		long sleepTime = 1000;
+		// String logLevelStr = System.getProperty("log.level");
+		// LogTimeLevel logLevel = LogTimeLevel.getByName(logLevelStr);
+		// PerformanceTimeLogger.setLogTimeLevel(logLevel);
 
 		try {
 			System.out.println("***************** Timer Logger Starts ******************");
+			PerformanceTimeLogger.getChronometer().start();
+
+			long sleepTime = 1000;
 			sleepMyThread(sleepTime);
-			System.out.println(PerformanceTimeLogger.getChronometer().stepElapsedTime("Mustafa"));
+			PerformanceTimeLogger.getChronometer().stepElapsedTime("Mustafa");
+
 			sleepTime = 2000;
 			sleepMyThread(sleepTime);
-			PerformanceTimeLogger.setLogTimeLevel(LogTimeLevel.HOURS);
+			PerformanceTimeLogger.getChronometer().stepElapsedTime("Kaptan");
 
-			System.out.println(PerformanceTimeLogger.getChronometer().stepElapsedTime("Kaptan"));
 			sleepTime = 3000;
 			sleepMyThread(sleepTime);
+			PerformanceTimeLogger.getChronometer().stepElapsedTime("Performance");
 
-			System.out.println(PerformanceTimeLogger.getChronometer().stepElapsedTime("Melike"));
 			sleepTime = 5000;
 			sleepMyThread(sleepTime);
+			PerformanceTimeLogger.getChronometer().stepElapsedTime("Tester");
 
-			System.out.println(PerformanceTimeLogger.getChronometer().stepElapsedTime("Yigit"));
-			System.out.println("***************** Timer Logger Step Logs ******************");
+			System.out.println("***************** Print Step Time Logs ******************");
 			System.out.println(PerformanceTimeLogger.getChronometer().getLinearizedLogs());
+
 			System.out.println("***************** Timer Logger Final Time ******************");
-			System.out.println(PerformanceTimeLogger.getChronometer().elapsedTime());
+			System.out.println(
+					TimerFormatterConstans.TOTAL_TIME + " : " + PerformanceTimeLogger.getChronometer().elapsedTime());
+
 			System.out.println("***************** Timer Logger Ends ******************");
-			System.out.println(PerformanceTimeLogger.getChronometer().getLinearizedLogs());
+			
+			
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
